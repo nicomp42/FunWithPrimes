528a809b2c76a0a7f95d25176e46f4f7970410fb
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-05-02 14:46:38 -0400

    Optimization

--- src/whoa/Main.java
+++ src/whoa/Main.java
@@ -2,6 +2,11 @@
  * Bill Nichlson
  * nicholdw@ucmail.uc.edu
  * Some test primes: https://primes.utm.edu/lists/small/small.html
+ * 
+ * 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 
+ * 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 
+ * 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+ * 
  */
 package whoa;
 
@@ -119,7 +124,7 @@ public class Main {
 		System.out.println("Result of comparison = " + comparison);
 */
 		BigInteger mod;
-		int startingPrime = 61;		// Where the divisor will start. 
+		int startingPrime = 71;		// Where the divisor will start. 
 		BigInteger divisor = new BigInteger(String.valueOf(startingPrime));
 		System.out.println("Starting test loop...");
 		startTime = System.currentTimeMillis();
@@ -129,9 +134,11 @@ public class Main {
 		BigInteger bigIntegerTwo = new BigInteger("2");
 		BigInteger num = myLKP.getNum();		// Create a local copy of the reference to the number we are checking for primeness.
 		BigInteger num_SquareRoot = myLKP.getMyNum_SquareRoot();	// Create a local copy of the reference to the square root
-		int checkCounter = startingPrime;
+		int checkCounter03 = startingPrime, checkCounter05 = startingPrime, checkCounter07 = startingPrime, checkCounter11 = startingPrime, checkCounter13 = startingPrime, checkCounter17 = startingPrime;
+		int checkCounter19 = startingPrime, checkCounter23 = startingPrime, checkCounter29 = startingPrime, checkCounter31 = startingPrime, checkCounter37 = startingPrime, checkCounter41 = startingPrime;
+		int checkCounter43 = startingPrime, checkCounter47 = startingPrime, checkCounter53 = startingPrime, checkCounter59 = startingPrime, checkCounter61 = startingPrime, checkCounter67 = startingPrime;
+		
 		// Check the first few primes. Make sure this stops at the last prime before the value of startingPrime
-		// 3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61, 
 		if            (num.mod(new BigInteger("02")).compareTo(BigInteger.ZERO) == 0) { System.out.println("******************* number is divisible by 2 *********************");
 			} else if (num.mod(new BigInteger("03")).compareTo(BigInteger.ZERO) == 0) { System.out.println("******************* number is divisible by 3 *********************");
 			} else if (num.mod(new BigInteger("05")).compareTo(BigInteger.ZERO) == 0) { System.out.println("******************* number is divisible by 5 *********************");
@@ -149,6 +156,8 @@ public class Main {
 			} else if (num.mod(new BigInteger("47")).compareTo(BigInteger.ZERO) == 0) {	System.out.println("******************* number is divisible by 47 *********************");
 			} else if (num.mod(new BigInteger("53")).compareTo(BigInteger.ZERO) == 0) {	System.out.println("******************* number is divisible by 53 *********************");
 			} else if (num.mod(new BigInteger("59")).compareTo(BigInteger.ZERO) == 0) {	System.out.println("******************* number is divisible by 59 *********************");
+			} else if (num.mod(new BigInteger("61")).compareTo(BigInteger.ZERO) == 0) {	System.out.println("******************* number is divisible by 61 *********************");
+			} else if (num.mod(new BigInteger("67")).compareTo(BigInteger.ZERO) == 0) {	System.out.println("******************* number is divisible by 67 *********************");
 			} else {
 				// We are out of tricks. Supposedly. Time to brute-force it, mostly
 				boolean checkThisDivisor = true;
@@ -159,22 +168,48 @@ public class Main {
 						if (mod.compareTo(BigInteger.ZERO) == 0) {System.out.println("******************* Divisor found *********************"); System.out.println(divisor.toString()); break;}
 						//System.out.println("i = " + i + " mod = " + mod.toString());
 
-						if (counter % 1_000_000 == 0) {
+						if (counter % 1_000 == 0) {
 							t2 = System.currentTimeMillis();
-							System.out.print(new DecimalFormat("#,#######.0000").format(((double)((t2 - t1))/1000)/60) + " minutes.");
-							//System.out.println(divisor.toString());
+							System.out.print(new DecimalFormat("#,#######.0000").format(((double)((t2 - t1))/1000)/60) + " minutes.   ");
 							System.out.print(" Divisor has " + divisor.toString().length() + " digits.");
-							System.out.println(new DecimalFormat(" Elapsed time = #,#######.0").format(((double)((t2 - startTime))/1000)/60) + " minutes.");
+							System.out.print(new DecimalFormat(" Elapsed time = #,#######.0").format(((double)((t2 - startTime))/1000)/60) + " minutes. divisor = ");
+							System.out.println(divisor.toString());	
 							counter = 0;
 							t1 = t2;
 						}
 						counter++;
 					}
 					divisor = divisor.add(bigIntegerTwo);	// Only check the odd numbers
-					checkCounter += 2;
+					checkCounter03 += 2; checkCounter05 += 2; checkCounter07 += 2; checkCounter11 += 2; checkCounter13 += 2; checkCounter17 += 2;
+					checkCounter19 += 2; checkCounter23 += 2; checkCounter29 += 2; checkCounter31 += 2; checkCounter37 += 2; checkCounter41 += 2;
+					checkCounter43 += 2; checkCounter47 += 2; checkCounter53 += 2; checkCounter59 += 2; checkCounter61 += 2; checkCounter67 += 2;
+					
 					checkThisDivisor = true;
-					if         (checkCounter % 3 == 0)  {checkThisDivisor = false;
-					} else 	if (checkCounter % 5 == 0)  {checkThisDivisor = false;
+					
+					// Have we added a multiple of a prime number to the divisor?
+					if (checkCounter03 %  3 == 0)  {checkThisDivisor = false; checkCounter03 = 0;}
+					if (checkCounter05 %  5 == 0)  {checkThisDivisor = false; checkCounter05 = 0;}
+					if (checkCounter07 %  7 == 0)  {checkThisDivisor = false; checkCounter07 = 0;}
+					if (checkCounter11 % 11 == 0)  {checkThisDivisor = false; checkCounter11 = 0;}
+					if (checkCounter13 % 13 == 0)  {checkThisDivisor = false; checkCounter13 = 0;}
+					if (checkCounter17 % 17 == 0)  {checkThisDivisor = false; checkCounter17 = 0;}
+
+					if (checkCounter19 % 19 == 0)  {checkThisDivisor = false; checkCounter19 = 0;}
+					if (checkCounter23 % 23 == 0)  {checkThisDivisor = false; checkCounter23 = 0;}
+					if (checkCounter29 % 29 == 0)  {checkThisDivisor = false; checkCounter29 = 0;}
+					if (checkCounter31 % 31 == 0)  {checkThisDivisor = false; checkCounter31 = 0;}
+					if (checkCounter37 % 37 == 0)  {checkThisDivisor = false; checkCounter37 = 0;}
+					if (checkCounter41 % 41 == 0)  {checkThisDivisor = false; checkCounter41 = 0;}
+					
+					if (checkCounter43 % 43 == 0)  {checkThisDivisor = false; checkCounter43 = 0;}
+					if (checkCounter47 % 47 == 0)  {checkThisDivisor = false; checkCounter47 = 0;}
+					if (checkCounter53 % 53 == 0)  {checkThisDivisor = false; checkCounter53 = 0;}
+					if (checkCounter59 % 59 == 0)  {checkThisDivisor = false; checkCounter59 = 0;}
+					if (checkCounter61 % 61 == 0)  {checkThisDivisor = false; checkCounter61 = 0;}
+					if (checkCounter67 % 67 == 0)  {checkThisDivisor = false; checkCounter67 = 0;}
+					/*
+					
+					else 	if (checkCounter % 5 == 0)  {checkThisDivisor = false;
 					} else 	if (checkCounter % 7 == 0)  {checkThisDivisor = false;
 					} else 	if (checkCounter % 11 == 0) {checkThisDivisor = false;
 					} else 	if (checkCounter % 13 == 0) {checkThisDivisor = false;
@@ -188,8 +223,10 @@ public class Main {
 					} else 	if (checkCounter % 43 == 0) {checkThisDivisor = false;
 					} else 	if (checkCounter % 47 == 0) {checkThisDivisor = false;
 					} else 	if (checkCounter % 53 == 0) {checkThisDivisor = false;
-					} else 	if (checkCounter % 59 == 0) {checkThisDivisor = false; checkCounter = 59; //System.out.println("found a divisor that is a factor of 59: checkCounter = " + checkCounter );
+					} else 	if (checkCounter % 59 == 0) {
+						checkThisDivisor = false; checkCounter = 59; //System.out.println("found a divisor that is a factor of 59: checkCounter = " + checkCounter );
 					}
+					*/
 					// Are we done?
 					// ToDo - we could check this every other iteration to speed it up.
 					if (divisor.compareTo(num_SquareRoot) > 0) {System.out.println("No divisor found, number is prime"); break;}
